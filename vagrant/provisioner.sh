92f57596a4d1fa9ac06eb0c6302a9d8416f41559
Jaco Sperelli <jacopo.mattia.sperelli@gmail.com>
2020-01-27 14:15:53 +0100

    added crdential

--- vagrant/provisioner.sh
+++ vagrant/provisioner.sh
@@ -3,8 +3,8 @@
 install_and_configure_git() {
     apk add git git-doc nano
     git config --system core.editor nano
-#    su vagrant -c "git config --global user.name \"<your_fullname_or_github_nickname\""
-#    su vagrant -c 'git config --global user.email <your_email@domain.test>'
+#    su vagrant -c "git config --global user.name \"jacopo\""
+#    su vagrant -c 'git config --global user.email abc@domain.test'
 }
 
 install_and_configure_docker() {

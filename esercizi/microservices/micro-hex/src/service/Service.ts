e78c4694ff283fb1d6c9c9e34d2e3a055423516b
Jaco Sperelli <jacopo.mattia.sperelli@gmail.com>
2020-03-17 16:13:51 +0100

    hex-to-rgb test passed

--- esercizi/microservices/micro-hex/src/service/Service.ts
+++ esercizi/microservices/micro-hex/src/service/Service.ts
@@ -1,8 +1,9 @@
 import colorConverter from 'color-convert'
+import { TtfRgb, TtfHex } from "../../../commons/src/model/Color"
 
 //TODO
-export function convert(color: ColorModel): ColorModel {
-    const colorToConvert: ColorModel = {};
-    const convertedColor = colorConverter;
-    return {} as ColorModel;
+export function convert(color: TtfHex): TtfRgb {
+    const colorToConvert: TtfHex = color;
+    const convertedColor = colorConverter.hex.rgb(colorToConvert.hex);
+    return {red:convertedColor[0], green:convertedColor[1], blue:convertedColor[2]} as TtfRgb;
 }

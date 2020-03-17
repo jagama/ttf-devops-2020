2742f8ab2c0330cff893a1921dc7d9eed28b3784
Jaco Sperelli <jacopo.mattia.sperelli@gmail.com>
2020-03-17 18:37:57 +0100

    rename

--- esercizi/microservices/micro-hex/src/service/Service.ts
+++
@@ -1,9 +0,0 @@
-import colorConverter from 'color-convert'
-import { TtfRgb, TtfHex } from "../../../commons/src/model/Color"
-
-//TODO
-export function convert(color: TtfHex): TtfRgb {
-    const colorToConvert: TtfHex = color;
-    const convertedColor = colorConverter.hex.rgb(colorToConvert.hex);
-    return {red:convertedColor[0], green:convertedColor[1], blue:convertedColor[2]} as TtfRgb;
-}
